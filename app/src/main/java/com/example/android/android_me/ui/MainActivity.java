3cfaaf24d6e68a16974fd2ff13a0382db0b4f99b
gadik <gadi.krn@gmail.com>
2018-06-19 12:32:56 +0300

    Add interface to the master fragment so it can communicate with the main activity. A toast shows the item selected position.

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.android_me.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.android_me.R;
 
@@ -10,11 +11,16 @@ import com.example.android.android_me.R;
  * Created by gadik
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MasterListFragment.onImageClickListener{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        Toast.makeText(this, "Item selected = " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -1,11 +1,13 @@
 package com.example.android.android_me.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.android.android_me.R;
@@ -17,8 +19,26 @@ import com.example.android.android_me.data.AndroidImageAssets;
 
 public class MasterListFragment extends Fragment {
 
+    onImageClickListener mCallback;
+
+    public interface onImageClickListener{
+        void onItemSelected(int position);
+    }
+
     public MasterListFragment(){
 
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // Check if the host activity implemented the interface
+        try{
+            mCallback = (onImageClickListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + " must implement onItemSelected");
+        }
     }
 
     @Nullable
@@ -30,6 +50,14 @@ public class MasterListFragment extends Fragment {
         // Set the adapter with all images to the grid view
         MasterListAdapter adapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
         imagesGridView.setAdapter(adapter);
+
+        // Trigger the callback onItemSelected when an item is clicked
+        imagesGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mCallback.onItemSelected(position);
+            }
+        });
 
         return rootView;
     }
